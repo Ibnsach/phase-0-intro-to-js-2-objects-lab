c8ddda19783866d267a1970243d6fa000934d57b
Adam Abass <adam.imoro@student.moringaschool.com>
2022-05-13 17:47:15 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+     name : "Adam",
+    streetAddress : "294 Bob",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+   newObject[key]=value;
+    return newObject;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee,key, value){
+    const newObject = employee;
+    newObject[key]=value;
+    return newObject;
+  }
+
+  function deleteFromEmployeeByKey(employee, key, value){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+  }
