377bc5568382530340fd1b57f1139e9ae01d5e01
Adam Abass <adam.imoro@student.moringaschool.com>
2022-05-13 11:58:32 +0000

    commit tree 31065cf241965f28d386ce82d45e9626051423b3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name : "Adam",
+    streetAddress : "NS-240-7726"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const Object = employee;
+    Object[key]=value;
+    return Object;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key){
+    const Object = {...employee};
+    delete Object[key];
+    return Object;
+  }
